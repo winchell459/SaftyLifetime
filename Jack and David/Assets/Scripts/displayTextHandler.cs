f6f52f49d7d4540fcc71da2709931f8b91cf1dd1
HoodieDave78 <59182874+HoodieDave78@users.noreply.github.com>
2020-01-03 06:31:45 +0800

    ｊｌ

---
+++ Jack and David/Assets/Scripts/displayTextHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayTextHandler : MonoBehaviour
+{
+    public GameObject[] TriggerEventObjects;
+    public GameObject text;
+    public bool ActivateText;
+    private bool triggered = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggered){
+            text.gameObject.SetActive(ActivateText);
+            foreach(GameObject triggerEvent in TriggerEventObjects){
+                triggerEvent.SendMessage("TriggerEvent"); 
+            }
+
+            //if(TriggerEventObject)TriggerEventObject.SendMessage("TriggerEvent");
+            triggered = false;
+        }
+    }
+
+    public void TriggerEvent(){
+        triggered = true;
+    }
+}
